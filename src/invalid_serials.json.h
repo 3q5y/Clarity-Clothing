cd9932954466f63f66cf065328171b785b1f194e
3q5y <107141565+3q5y@users.noreply.github.com>
2020-09-28 12:18:22 +0000

    not done

---
+++ src/invalid_serials.json.h
@@ -0,0 +1,36 @@
+// Copyright (c) 2017 The PIVX developers
+// Copyright (c) 2018-2019 The GIANT developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef GIANT_INVALID_SERIALS_JSON_H
+#define GIANT_INVALID_SERIALS_JSON_H
+
+#include <string>
+
+std::string LoadInvalidSerials()
+{
+    std::string str = "[\n"
+            "  {\n"
+            "    \"s\": \"c9c868bb56eacfc4f3d829528a0ae812dff26619cd38e6c9a0eea1eacddc84\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"17ab90c682a42d7a3b8bd1da019f6e788e82f25220a1cdaca242f15873ee553\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"1aca2f77af39229578b4093855278a20569ae7ea81eeda1263eac8c2cc9fcf7\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"4f3e09eab1981c715af9ec05d9c13bef831501250dbdd477c429329e837abf1\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"62c55983e9c14261f52eb6c22a154ea0e1534a938ce97ab17d5d057bbda7d2d\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"7075dcfbbcc32b6a871244635155b4f0a95146d7c0171c4eb5667120ab4d173\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"7395cc3cc468e7e7c81f254fedfcc800089846f3eb34c9ba7846e8464396848\"\n"
+            "  },\n"
+            "  {\n"
+            "    \"s\": \"
