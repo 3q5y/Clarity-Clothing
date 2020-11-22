cbfd58309392d76979c4077585e50c452e612d2a
3q5y <107141565+3q5y@users.noreply.github.com>
2020-11-22 20:49:51 +0000

    docstring update

--- src/libzerocoin/AccumulatorProofOfKnowledge.cpp
+++ src/libzerocoin/AccumulatorProofOfKnowledge.cpp
@@ -120,4 +120,26 @@ bool AccumulatorProofOfKnowledge:: Verify(const Accumulator& a, const CBigNum& v
 	CBigNum c = CBigNum(hasher.GetHash()); //this hash should be of length k_prime bits
 
 	CBigNum st_1_prime = (valueOfCommitmentToCoin.pow_mod(c, params->accumulatorPoKCommitmentGroup.modulus) * sg.pow_mod(s_alpha, params->accumulatorPoKCommitmentGroup.modulus) * sh.pow_mod(s_phi, params->accumulatorPoKCommitmentGroup.modulus)) % params->accumulatorPoKCommitmentGroup.modulus;
-	CBigNum st_2_prime = (sg.pow_mod(c, params->accumulatorPoKCommitmentGroup.modulus) * ((valueOfCommitmentToCoin * sg.inverse(params->accumulatorPoKCommitmentGroup.modulus)).pow_mod(s_gamma, params->accumulatorPoKCommitmentGroup.modulus)) * sh.pow_mod(s_psi, params->accumulatorPoKCommitmentGrou
+	CBigNum st_2_prime = (sg.pow_mod(c, params->accumulatorPoKCommitmentGroup.modulus) * ((valueOfCommitmentToCoin * sg.inverse(params->accumulatorPoKCommitmentGroup.modulus)).pow_mod(s_gamma, params->accumulatorPoKCommitmentGroup.modulus)) * sh.pow_mod(s_psi, params->accumulatorPoKCommitmentGroup.modulus)) % params->accumulatorPoKCommitmentGroup.modulus;
+	CBigNum st_3_prime = (sg.pow_mod(c, params->accumulatorPoKCommitmentGroup.modulus) * (sg * valueOfCommitmentToCoin).pow_mod(s_sigma, params->accumulatorPoKCommitmentGroup.modulus) * sh.pow_mod(s_xi, params->accumulatorPoKCommitmentGroup.modulus)) % params->accumulatorPoKCommitmentGroup.modulus;
+
+	CBigNum t_1_prime = (C_r.pow_mod(c, params->accumulatorModulus) * h_n.pow_mod(s_zeta, params->accumulatorModulus) * g_n.pow_mod(s_epsilon, params->accumulatorModulus)) % params->accumulatorModulus;
+	CBigNum t_2_prime = (C_e.pow_mod(c, params->accumulatorModulus) * h_n.pow_mod(s_eta, params->accumulatorModulus) * g_n.pow_mod(s_alpha, params->accumulatorModulus)) % params->accumulatorModulus;
+	CBigNum t_3_prime = ((a.getValue()).pow_mod(c, params->accumulatorModulus) * C_u.pow_mod(s_alpha, params->accumulatorModulus) * ((h_n.inverse(params->accumulatorModulus)).pow_mod(s_beta, params->accumulatorModulus))) % params->accumulatorModulus;
+	CBigNum t_4_prime = (C_r.pow_mod(s_alpha, params->accumulatorModulus) * ((h_n.inverse(params->accumulatorModulus)).pow_mod(s_delta, params->accumulatorModulus)) * ((g_n.inverse(params->accumulatorModulus)).pow_mod(s_beta, params->accumulatorModulus))) % params->accumulatorModulus;
+
+	bool result_st1 = (st_1 == st_1_prime);
+	bool result_st2 = (st_2 == st_2_prime);
+	bool result_st3 = (st_3 == st_3_prime);
+
+	bool result_t1 = (t_1 == t_1_prime);
+	bool result_t2 = (t_2 == t_2_prime);
+	bool result_t3 = (t_3 == t_3_prime);
+	bool result_t4 = (t_4 == t_4_prime);
+
+	bool result_range = ((s_alpha >= -(params->maxCoinValue * CBigNum(2).pow(params->k_prime + params->k_dprime + 1))) && (s_alpha <= (params->maxCoinValue * CBigNum(2).pow(params->k_prime + params->k_dprime + 1))));
+
+    return result_st1 && result_st2 && result_st3 && result_t1 && result_t2 && result_t3 && result_t4 && result_range;
+}
+
+} /* namespace libzerocoin */
---
+++ src/libzerocoin/Coin.cpp
@@ -0,0 +1,182 @@
+/**
+ * @file       Coin.cpp
+ *
+ * @brief      PublicCoin and PrivateCoin classes for the Zerocoin library.
+ *
+ * @author     Ian Miers, Christina Garman and Matthew Green
+ * @date       June 2013
+ *
+ * @copyright  Copyright 2013 Ian Miers, Christina Garman and Matthew Green
+ * @license    This project is released under the MIT license.
+ **/
+// Copyright (c) 2017-2018 The PIVX developers
+// Copyright (c) 2018-2019 The GIANT developers
+
+#include <stdexcept>
+#include <iostream>
+#include "Coin.h"
+#include "Commitment.h"
+#include "pubkey.h"
+
+namespace libzerocoin {
+
+//PublicCoin class
+PublicCoin::PublicCoin(const ZerocoinParams* p):
+	params(p) {
+	if (this->params->initialized == false) {
+		throw std::runtime_error("Params are not initialized");
+	}
+    // Assume this will get set by another method later
+    denomination = ZQ_ERROR;
+};
+
+PublicCoin::PublicCoin(const ZerocoinParams* p, const CBigNum& coin, const CoinDenomination d):
+	params(p), value(coin) {
+	if (this->params->initialized == false) {
+		throw std::runtime_error("Params are not initialized");
+	}
+
+	denomination = d;
+	for(const CoinDenomination denom : zerocoinDenomList) {
+		if(denom == d)
+			denomination = d;
+	}
+    if (denomination == 0) {
+		std::cout << "denom does not exist\n";
+		throw std::runtime_error("Denomination does not exist");
+	}
+};
+
+bool PublicCoin::validate() const
+{
+    if (this->params->accumulatorParams.minCoinValue >= value) {
+        return error("%s: ERROR: PublicCoin::validate value is too low: %s", __func__, value.GetDec());
+    }
+
+    if (value > this->params->accumulatorParams.maxCoinValue) {
+        return error("%s: ERROR: PublicCoin::validate value is too high, max: %s, received: %s",
+                __func__, this->params->accumulatorParams.maxCoinValue, value.GetDec());
+    }
+
+    if (!value.isPrime(params->zkp_iterations)) {
+        return error("%s: ERROR: PublicCoin::validate value is not prime. Value: %s, Iterations: %d",
+                __func__, value.GetDec(), params->zkp_iterations);
+    }
+
+    return true;
+}
+
+//PrivateCoin class
+PrivateCoin::PrivateCoin(const ZerocoinParams* p, const CoinDenomination denomination, bool fMintNew): params(p), publicCoin(p) {
+	// Verify that the parameters are valid
+	if(this->params->initialized == false) {
+		throw std::runtime_error("Params are not initialized");
+	}
+
+    if (fMintNew) {
+#ifdef ZEROCOIN_FAST_MINT
+        // Mint a new coin with a random serial number using the fast process.
+        // This is more vulnerable to timing attacks so don't mint coins when
+        // somebody could be timing you.
+        this->mintCoinFast(denomination);
+#else
+        // Mint a new coin with a random serial number using the standard process.
+        this->mintCoin(denomination);
+#endif
+    }
+
+    this->version = CURRENT_VERSION;
+}
+
+PrivateCoin::PrivateCoin(const ZerocoinParams* p, const CoinDenomination denomination, const CBigNum& bnSerial,
+                         const CBigNum& bnRandomness): params(p), publicCoin(p)
+{
+        // Verify that the parameters are valid
+    if(!this->params->initialized)
+        throw std::runtime_error("Params are not initialized");
+
+    this->serialNumber = bnSerial;
+    this->randomness = bnRandomness;
+
+    Commitment commitment(&p->coinCommitmentGroup, bnSerial, bnRandomness);
+    this->publicCoin = PublicCoin(p, commitment.getCommitmentValue(), denomination);
+}
+
+bool PrivateCoin::IsValid()
+{
+    if (!IsValidSerial(params, serialNumber)) {
+        cout << "Serial not valid\n";
+        return false;
+    }
+
+    return getPublicCoin().validate();
+}
+
+bool GenerateKeyPair(const CBigNum& bnGroupOrder, const uint256& nPrivkey, CKey& key, CBigNum& bnSerial)
+{
+    // Generate a new key pair, which also has a 256-bit pubkey hash that qualifies as a serial #
+    // This builds off of Tim Ruffing's work on libzerocoin, but has a different implementation
+    CKey keyPair;
+    if (nPrivkey == 0)
+        keyPair.MakeNewKey(true);
+    else
+        keyPair.Set(nPrivkey.begin(), nPrivkey.end(), true);
+
+    CPubKey pubKey = keyPair.GetPubKey();
+    uint256 hashPubKey = Hash(pubKey.begin(), pubKey.end());
+
+    // Make the first half byte 0 which will distinctly mark v2 serials
+    hashPubKey >>= libzerocoin::PrivateCoin::V2_BITSHIFT;
+
+    CBigNum s(hashPubKey);
+    uint256 nBits = hashPubKey >> 248; // must be less than 0x0D to be valid serial range
+    if (nBits > 12)
+        return false;
+
+    //Mark this as v2 by starting with 0xF
+    uint256 nMark = 0xF;
+    nMark <<= 252;
+    hashPubKey |= nMark;
+    s = CBigNum(hashPubKey);
+
+    key = keyPair;
+    bnSerial = s;
+    return true;
+}
+
+const CPubKey PrivateCoin::getPubKey() const
+{
+	CKey key;
+	key.SetPrivKey(privkey, true);
+	return key.GetPubKey();
+}
+
+bool PrivateCoin::sign(const uint256& hash, vector<unsigned char>& vchSig) const
+{
+	CKey key;
+	key.SetPrivKey(privkey, true);
+	return key.Sign(hash, vchSig);
+}
+
+void PrivateCoin::mintCoin(const CoinDenomination denomination) {
+	// Repeat this process up to MAX_COINMINT_ATTEMPTS times until
+	// we obtain a prime number
+	for(uint32_t attempt = 0; attempt < MAX_COINMINT_ATTEMPTS; attempt++) {
+
+		// Generate a random serial number in the range 0...{q-1} where
+		// "q" is the order of the commitment group.
+		// And where the serial also doubles as a public key
+		CKey key;
+		CBigNum s;
+        bool isValid = false;
+        while (!isValid) {
+            isValid = GenerateKeyPair(this->params->coinCommitmentGroup.groupOrder, uint256(0), key, s);
+        }
+
+		// Generate a Pedersen commitment to the serial number "s"
+		Commitment coin(&params->coinCommitmentGroup, s);
+
+		// Now verify that the commitment is a prime number
+		// in the appropriate range. If not, we'll throw this coin
+		// away and generate a new one.
+		if (coin.get
