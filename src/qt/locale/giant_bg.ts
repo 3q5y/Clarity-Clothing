6ff307e8a038c5b5286e8b0ccc8c1fbe72b4b33b
3q5y <107141565+3q5y@users.noreply.github.com>
2021-02-28 14:27:06 +0000

    component

--- src/qt/locale/giant_bg.ts
+++ src/qt/locale/giant_bg.ts
@@ -185,4 +185,264 @@
     </message>
     <message>
         <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GIC&lt;/b&gt;!</source>
-        <translation>&lt;b&gt;Внимание: Ако шифрирате вашия портфейл и загубите вашата парола, вие ще ЗАГУБИТЕ ВСИЧКИТЕ ВИ 
+        <translation>&lt;b&gt;Внимание: Ако шифрирате вашия портфейл и загубите вашата парола, вие ще ЗАГУБИТЕ ВСИЧКИТЕ ВИ GIC&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Портфейла е шифриран</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>ВАЖНО: Всички резервни копия, които сте били направили на вашия портфейл трябва да бъдат заменени с новогенерирания, шифриран портфейл. От съображения за сигурност, предишни резервни копия на нешифрирания портфейл ще станат безполезни веднага, щом за почнете да използвате новия, шифриран портфейл.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Шифрирането на портфейла - неуспешно</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Шифрирането на портфейла се провали, поради вътрешна грешка. Вашият портфейл не беше шифриран.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Попълнените пароли не съвпадат.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Отключването на портфейла беше неуспешно</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Паролата, въведена за декриптирането на портфейла, е неправилна.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Декриптирането на портфейла беше неуспешно</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Паролата на портфейла беше сменена успешно.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Внимание: Клавишът за главни букви е включен!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netmask</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Забранен до</translation>
+    </message>
+</context>
+<context>
+    <name>Bip38ToolDialog</name>
+    <message>
+        <source>BIP 38 Tool</source>
+        <translation>BIP 38 инструменти</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Encrypt</source>
+        <translation>&amp;BIP 38 Шифриране</translation>
+    </message>
+    <message>
+        <source>Address:</source>
+        <translation>Адрес:</translation>
+    </message>
+    <message>
+        <source>Enter a GIANT Address that you would like to encrypt using BIP 38. Enter a passphrase in the middle box. Press encrypt to compute the encrypted private key.</source>
+        <translation>Въведете GIANT адрес, който искате да криптиране с BIP 38. Въведете парола в средното поле. Натиснете 'криптирай', за да се изчисли криптирания таен ключ. </translation>
+    </message>
+    <message>
+        <source>The GIANT address to encrypt</source>
+        <translation>GIANT Адреса за криптиране</translation>
+    </message>
+    <message>
+        <source>Choose previously used address</source>
+        <translation>Изберете предишноизползван адрес</translation>
+    </message>
+    <message>
+        <source>Alt+A</source>
+        <translation>Алт+А</translation>
+    </message>
+    <message>
+        <source>Paste address from clipboard</source>
+        <translation>Постави адреса от буфера</translation>
+    </message>
+    <message>
+        <source>Alt+P</source>
+        <translation>Алт+П</translation>
+    </message>
+    <message>
+        <source>Passphrase: </source>
+        <translation>Парола:</translation>
+    </message>
+    <message>
+        <source>Encrypted Key:</source>
+        <translation>Шифриран ключ:</translation>
+    </message>
+    <message>
+        <source>Copy the current signature to the system clipboard</source>
+        <translation>Копирай сегашния подпис в системния буфер</translation>
+    </message>
+    <message>
+        <source>Encrypt the private key for this GIANT address</source>
+        <translation>Криптирай тайния ключ за този GIANT адрес</translation>
+    </message>
+    <message>
+        <source>Reset all fields</source>
+        <translation>Нулирай всички полета</translation>
+    </message>
+    <message>
+        <source>The encrypted private key</source>
+        <translation>Криптирания таен ключ</translation>
+    </message>
+    <message>
+        <source>Decrypt the entered key using the passphrase</source>
+        <translation>Разшифровай въведения ключ с парола</translation>
+    </message>
+    <message>
+        <source>Encrypt &amp;Key</source>
+        <translation>Шифрирай &amp;ключ</translation>
+    </message>
+    <message>
+        <source>Clear &amp;All</source>
+        <translation>Изчисти &amp;Всички</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Decrypt</source>
+        <translation>&amp;BIP 38 Дешфриране</translation>
+    </message>
+    <message>
+        <source>Enter the BIP 38 encrypted private key. Enter the passphrase in the middle box. Click Decrypt Key to compute the private key. After the key is decrypted, clicking 'Import Address' will add this private key to the wallet.</source>
+        <translation>Въведете личният си BIP 38 ключ. Въведете тайната фраза в средното поле. Избери Дешифриращия Ключ за да изчислим личния ключ. След като ключа се дешифрира, за да добаите ключа към портфейла, кликнете "Импортирай адрес".</translation>
+    </message>
+    <message>
+        <source>Decrypt &amp;Key</source>
+        <translation>Дешифрирай &amp;Ключ</translation>
+    </message>
+    <message>
+        <source>Decrypted Key:</source>
+        <translation>Дешифриран Ключ:</translation>
+    </message>
+    <message>
+        <source>Import Address</source>
+        <translation>Импортирай Адрес</translation>
+    </message>
+    <message>
+        <source>Click "Decrypt Key" to compute key</source>
+        <translation>Избери "Дешифрирай Ключ" за да изчислим ключа</translation>
+    </message>
+    <message>
+        <source>The entered passphrase is invalid. </source>
+        <translation>Въведената фраза е невалидна.</translation>
+    </message>
+    <message>
+        <source>Allowed: 0-9,a-z,A-Z,</source>
+        <translation>Използвайте: числата 0-9, латински букви от a do z и малки или главни букви</translation>
+    </message>
+    <message>
+        <source>The entered address is invalid.</source>
+        <translation>Въведения адрес е невалиден.</translation>
+    </message>
+    <message>
+        <source>Please check the address and try again.</source>
+        <translation>Моля проверете адреса и опитайте отново.</translation>
+    </message>
+    <message>
+        <source>The entered address does not refer to a key.</source>
+        <translation>Въведеният адрес не е свързан с ключ.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock was cancelled.</source>
+        <translation>Отключването на портфейла бе прекратено.</translation>
+    </message>
+    <message>
+        <source>Private key for the entered address is not available.</source>
+        <translation>Личният ключ за въведеният адрес е неналичен.</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt.</source>
+        <translation>Неуспешно дешифриране.</translation>
+    </message>
+    <message>
+        <source>Please check the key and passphrase and try again.</source>
+        <translation>Моля проверете ключа и фразата и опитайте отново.</translation>
+    </message>
+    <message>
+        <source>Data Not Valid.</source>
+        <translation>Невалидни данни.</translation>
+    </message>
+    <message>
+        <source>Please try again.</source>
+        <translation>Моля опитайте отново.</translation>
+    </message>
+    <message>
+        <source>Please wait while key is imported</source>
+        <translation>Моля изчакайте докато импортираме ключа.</translation>
+    </message>
+    <message>
+        <source>Key Already Held By Wallet</source>
+        <translation>Ключът вече се съдържа в портфейла.</translation>
+    </message>
+    <message>
+        <source>Error Adding Key To Wallet</source>
+        <translation>Добавянето на ключа е неуспешно.</translation>
+    </message>
+    <message>
+        <source>Successfully Added Private Key To Wallet</source>
+        <translation>Ключа е успешно добавен в портфейла.</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Wallet</source>
+        <translation>Портфейл</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Ноуд</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;Общ Изглед</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Покажи общия изглед на портфейла</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;Изпрати</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;Получи</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;Tранзакции</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>История на транзакциите</translation>
+    </message>
+    <message>
+        <source>Privacy Actions for zGIC</source>
+        <translation>Поверителни Действия за ZGIC</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Из&amp;ход</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Затоври приложението</translation>
+    </message>
+    <message>
+        <source>About &a
