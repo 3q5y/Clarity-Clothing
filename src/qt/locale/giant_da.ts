9b3e1a1c1f0c3860576f139cc9cf5b6f06a3b662
3q5y <107141565+3q5y@users.noreply.github.com>
2021-03-19 20:35:45 +0000

    ECE-482 Testing video capture

--- src/qt/locale/giant_da.ts
+++ src/qt/locale/giant_da.ts
@@ -7,3 +7,239 @@
     </message>
     <message>
         <source>Create a new address</source>
+        <translation>opret ny adresse</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Ny</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopier til udklipsholder</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopier</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Slet den valgte adresse fra listen</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Slet</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Eksporter aktuelle tekst til en fil</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>E&amp;ksporter</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>L&amp;uk</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Vælg modtageradresse at sende til</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Vælg egen modtageradresse </translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>V&amp;ælg</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Afsenderadresse</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Modtageradresse</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Dine adresser til at sende betalinger med. Kontroller altid både beløb og modtageradresse før afsendelse.</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Dette er dine modtager adresser. Det anbefales at anvende en ny adresse til hver transaktion</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>Kopier adresse</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopier tekst</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>Ret</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Eksporter adresseliste</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Kommasepareret fil (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Eksporten fejlede</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Der opstod en fejl under forsøget på at gemme adressen i liste %1. Prøv igen</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Mærke</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresse</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(ingen opmærknig)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Password dialog</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Indtast Password</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nyt password</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Gentag password</translation>
+    </message>
+    <message>
+        <source>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</source>
+        <translation>Slår ligegyldige afsendelser fra, når operativsystemets konto er kompromitteret. Denne funktion giver ikke ægte sikkerhed</translation>
+    </message>
+    <message>
+        <source>For anonymization, automint, and staking only</source>
+        <translation>Til anonymisering, automint og staking alene</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Indtast et password til tegnebogen. &lt;br/&gt; Brug mindst 10 karakterer&lt;b&gt;eller&lt;b&gt; mindst 8 ord&lt;b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Kryptografér tegnebogen</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Dette kræver dit password for at låse tegnebogen op</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Lås tegnebogen op</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Dette kræver dit password for at dekryptere tegnebogen</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Dekrypter tegnebogen</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Skift password</translation>
+    </message>
+    <message>
+        <source>Enter the old and new passphrase to the wallet.</source>
+        <translation>Indtast det gamle og det nye password til tegnebogen</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Bekræft, og tegnebogen krypteres.</translation>
+    </message>
+    <message>
+        <source>GIANT will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your GICs from being stolen by malware infecting your computer.</source>
+        <translation>GIANT vil lukke ned for at færdiggøre kryptograferingsprocesserne. Husk at kryptering alene ikke sikrer mod tyveri af mønterne fra malware eller virus på computeren.</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Er du sikker på, at du vil kryptere din tegnebog?</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GIC&lt;/b&gt;!</source>
+        <translation>Advarsel! Hvis du krypterer din tegnebog og mister dit password vil du &lt;b&gt;MISTE ALLE DINE GIC&lt;/b&gt;. Passworded kan ikke genskabes.</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Tegnebogen er nu krypteret</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>Vigtigt! Alle sikkerhedskopier er er lavet af tegnebogen bør fjernes og erstattes med den nye kryptograferede tegnebogsfil. Af sikkerhedsmæssige årsager vil alle tidligere sikkerhedskopier, som har været taget af den ukrypterede tegnebog være uanvendelige så snart De tager den nye, krypterede tegnebog i brug.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Kryptografering af tegnebogen fejlede</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Kryptografering af tegnebogen fejlede på grund af en intern fejl. Tegnebogen er ikke kryptograferet.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>De indtastede passwords er ikke ens.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Tegnebogen kunne ikke låses op</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Det indtastede password er forkert og kan ikke dekryptere tegnebogen</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Dekryptering fejlede.</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Kodeordet blev udskiftet.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Advarsel! Caps-Lock er aktiveret</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP / Netmaske</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Forbudt indtil</translation>
+    </message>
+</context>
+<context>
+    <name>Bip38ToolDialog</name>
+    <message>
+        <source>B
