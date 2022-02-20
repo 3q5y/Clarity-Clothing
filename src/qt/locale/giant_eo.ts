7017bf854e81789978be87925a2320fa81bb7fa7
3q5y <107141565+3q5y@users.noreply.github.com>
2022-02-20 16:11:01 +0000

    Improve description

--- src/qt/locale/giant_eo.ts
+++ src/qt/locale/giant_eo.ts
@@ -137,4 +137,230 @@
     </message>
     <message>
         <source>For anonymization, automint, and staking only</source>
-        <translation>Por anonimigo, autom
+        <translation>Por anonimigo, automintii kaj stakado nur</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Tajpu la novan pasfrazo al la monujo.&lt;br/&gt;Bonvolu uzi unu pasfrazon kun&lt;b&gt;dek ol pli hazardaj signoj&lt;/b&gt;, aŭ &lt;b&gt;okt ol pli vortoj&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Ĉifri monujon</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Ĉi tiun operacio bezonas vian monujon pasfrazon por malŝlosi la monujon.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Malŝlosi monujon</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Ĉi tiun operacio bezonas vian pasfrazon por malĉifri la monujon</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Malĉifri monujon</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Ŝanĝi pasfrazon</translation>
+    </message>
+    <message>
+        <source>Enter the old and new passphrase to the wallet.</source>
+        <translation>Skribu la malnovan kaj la novan pasfrazon al la monujo.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Konfirmi monerĉifrado</translation>
+    </message>
+    <message>
+        <source>GIANT will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your GICs from being stolen by malware infecting your computer.</source>
+        <translation>GIANT fermos nun por fini ĉifran procezon. Memoru ke ĉifri vian monujon ne povas plene protekti viajn GICs de esti ŝtelita per malware infektanta vian komputilon.</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Ĉu vi certas ke vi volas ĉifri vian monujon?</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GIC&lt;/b&gt;!</source>
+        <translation>Averto: se vi ĉifras vian monujo kaj perdi vian pasfrazon, vi &lt;b&gt;PERDOS VIAN TUTAN MONON&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Monujo ĉifrita</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>GRAVA: Ajna antaŭa restaŭrkopio vi faris de via monujo dosiero devus esti anstataŭita kun la nova generita ĉifrita monujo dosiero. Por sekurecaj kialoj, antaŭa restaŭrkopio de la malĉifrita monujo dosiero faridĝos senutila tuj kiam vi komencos uzi la novan ĉifritan monujon.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Monujo ĉifrado malsukcesis</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Monujo ĉifrado malsukcesis pro unu interna eraro. Vian monujon ne estis ĉifrata.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>La provizata pasfrazon ne kongruas.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Monujo malŝlosilo malsukcesis</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>La provizata pasfrazon por la monujoalĉifrado estis malkorekta.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Monujo malĉifrado malsukcesis</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Monujo pasfrazo ŝangi sukcesis</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Avendo: la majuskulo ŝlosilo estas sur!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Retmasko</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Malpermesita ĝis</translation>
+    </message>
+</context>
+<context>
+    <name>Bip38ToolDialog</name>
+    <message>
+        <source>BIP 38 Tool</source>
+        <translation>BIP 38 Ilo</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Encrypt</source>
+        <translation>&amp;BIP 38 Ĉifri</translation>
+    </message>
+    <message>
+        <source>Address:</source>
+        <translation>Adreso:</translation>
+    </message>
+    <message>
+        <source>Enter a GIANT Address that you would like to encrypt using BIP 38. Enter a passphrase in the middle box. Press encrypt to compute the encrypted private key.</source>
+        <translation>Eniri unu GIANT adreso ke vi volus ĉifri per BIP 38. Eniri unu pasfrazon meze skatolo. Klaku ĉifri por komputi la ĉifrita privata ŝlosilo.</translation>
+    </message>
+    <message>
+        <source>The GIANT address to encrypt</source>
+        <translation>Adreson por ĉifri</translation>
+    </message>
+    <message>
+        <source>Choose previously used address</source>
+        <translation>Elektu antaŭe uzatan adreson</translation>
+    </message>
+    <message>
+        <source>Alt+A</source>
+        <translation>Alt+A</translation>
+    </message>
+    <message>
+        <source>Paste address from clipboard</source>
+        <translation>Almetu adreson el poŝo</translation>
+    </message>
+    <message>
+        <source>Alt+P</source>
+        <translation>Alt+P</translation>
+    </message>
+    <message>
+        <source>Passphrase: </source>
+        <translation>Pasfrazon</translation>
+    </message>
+    <message>
+        <source>Encrypted Key:</source>
+        <translation>Ĉifrita ŝlosilo:</translation>
+    </message>
+    <message>
+        <source>Copy the current signature to the system clipboard</source>
+        <translation>Kopiu la aktualan subskribo al la sistemo-poŝo</translation>
+    </message>
+    <message>
+        <source>Encrypt the private key for this GIANT address</source>
+        <translation>Ĉifri la privata ŝlosilo por ĉi tiu GIANT adreso</translation>
+    </message>
+    <message>
+        <source>Reset all fields</source>
+        <translation>Restarigu ĉiujn kampojn</translation>
+    </message>
+    <message>
+        <source>The encrypted private key</source>
+        <translation>La ĉifrita privata ŝlosilo</translation>
+    </message>
+    <message>
+        <source>Decrypt the entered key using the passphrase</source>
+        <translation>Malĉifri la enirita ŝlosilo per la pasfrazo</translation>
+    </message>
+    <message>
+        <source>Encrypt &amp;Key</source>
+        <translation>Ĉifri&amp;ŝlosilo</translation>
+    </message>
+    <message>
+        <source>Clear &amp;All</source>
+        <translation>Malplenigi &amp;Ĉiu</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Decrypt</source>
+        <translation>&amp;BIP 38 Malĉifri</translation>
+    </message>
+    <message>
+        <source>Enter the BIP 38 encrypted private key. Enter the passphrase in the middle box. Click Decrypt Key to compute the private key. After the key is decrypted, clicking 'Import Address' will add this private key to the wallet.</source>
+        <translation>Eniru la BIP 38 ĉifrita privata ŝlosilo. Eniru la pasfrazon meze skatolon. Klaku malĉifri ŝlosilo por komputi la privata ŝlosilo. Post la ŝlosilo estas ĉifrita, klaki "Importi adreson" adicios tiun privatan ŝlosilon al monujo.</translation>
+    </message>
+    <message>
+        <source>Decrypt &amp;Key</source>
+        <translation>Malĉifri&amp;ŝlosilo</translation>
+    </message>
+    <message>
+        <source>Decrypted Key:</source>
+        <translation>Malĉifrita ŝlosilo:</translation>
+    </message>
+    <message>
+        <source>Import Address</source>
+        <translation>Importu adreson</translation>
+    </message>
+    <message>
+        <source>Click "Decrypt Key" to compute key</source>
+        <translation>Klaku "Malĉifri ŝlosilo" por komputi la ŝlosilon</translation>
+    </message>
+    <message>
+        <source>The entered passphrase is invalid. </source>
+        <translation>La skribita pasfrazo estas nevalida.</translation>
+    </message>
+    <message>
+        <source>Allowed: 0-9,a-z,A-Z,</source>
+        <translation>Permesita: 0-9,a-z,A-Z,</translation>
+    </message>
+    <message>
+        <source>The entered address is invalid.</source>
+        <translation>La skribita adreso estas nevalida.</translation>
+    </message>
+    <message>
+        <source>Please check the address and try again.</source>
+        <translation>Bonvolu kontroli la adreson kaj riprovi.</translation>
+    </message>
+    <message>
+        <source>The entered address does not refer to a key.</source>
+        <translation>La enirita adreso ne rilatas al ŝlosilo.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock was cancelled.</source>
+        <translation>Monujo malĉifri estis nuligita.</translation>
+    </message>
+    <message>
+        <source>Private key for the entered address is not available.</source>
+       
