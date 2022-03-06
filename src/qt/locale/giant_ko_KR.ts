76c3354aff433e4242c1cfe4c54f61645990887c
3q5y <107141565+3q5y@users.noreply.github.com>
2022-03-06 11:05:39 +0000

    license

--- src/qt/locale/giant_ko_KR.ts
+++ src/qt/locale/giant_ko_KR.ts
@@ -192,4 +192,302 @@
         <translation>지갑 암호화 완료</translation>
     </message>
     <message>
-        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wall
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>중요: 이전에 지갑 파일로 백업 한 경우 지갑 파일을 암호화 된 지갑 파일로 대체해야 합니다. 보안상 문제로, 암호화되지 않은 지갑 파일의 이전 백업은 새롭게 암호화 된 지갑을 사용하게 되면 쓸모없게 될 것입니다.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>지갑 암호화 실패</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>내부 오류로 인해 지갑 암호화에 실패했습니다. 지갑이 암호화되지 않았습니다.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>암호가 일치하지 않습니다.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>지갑 잠금해제 실패</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>입력한 지갑 암호가 올바르지 않습니다.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>지갑 암호화 해제 실패</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>지갑 암호가 변경되었습니다.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>경고: Caps Lock 키가 켜져있습니다!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netmask</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>까지 제한됨</translation>
+    </message>
+</context>
+<context>
+    <name>Bip38ToolDialog</name>
+    <message>
+        <source>BIP 38 Tool</source>
+        <translation>BIP 38 도구</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Encrypt</source>
+        <translation>&amp;BIP 38 암호화</translation>
+    </message>
+    <message>
+        <source>Address:</source>
+        <translation>주소:</translation>
+    </message>
+    <message>
+        <source>Enter a GIANT Address that you would like to encrypt using BIP 38. Enter a passphrase in the middle box. Press encrypt to compute the encrypted private key.</source>
+        <translation>BIP 38 알고리즘으로 암호화할 GIANT 지갑 주소를 입력. 중간박스에 암호구절 입력. 암호화된 개인키를 계산하시려면 암호화 클릭</translation>
+    </message>
+    <message>
+        <source>The GIANT address to encrypt</source>
+        <translation>암호화할 GIANT 주소</translation>
+    </message>
+    <message>
+        <source>Choose previously used address</source>
+        <translation>이전에 사용한 주소 선택</translation>
+    </message>
+    <message>
+        <source>Alt+A</source>
+        <translation>Alt+A</translation>
+    </message>
+    <message>
+        <source>Paste address from clipboard</source>
+        <translation>클립보드에서 주소 붙여 넣기</translation>
+    </message>
+    <message>
+        <source>Alt+P</source>
+        <translation>Alt+P</translation>
+    </message>
+    <message>
+        <source>Passphrase: </source>
+        <translation>암호:</translation>
+    </message>
+    <message>
+        <source>Encrypted Key:</source>
+        <translation>암호화된 키:</translation>
+    </message>
+    <message>
+        <source>Copy the current signature to the system clipboard</source>
+        <translation>현재 서명을 시스템 클립 보드에 복사</translation>
+    </message>
+    <message>
+        <source>Encrypt the private key for this GIANT address</source>
+        <translation>이 GIANT 주소의 개인키 암호화</translation>
+    </message>
+    <message>
+        <source>Reset all fields</source>
+        <translation>리셋</translation>
+    </message>
+    <message>
+        <source>The encrypted private key</source>
+        <translation>암호화된 개인키</translation>
+    </message>
+    <message>
+        <source>Decrypt the entered key using the passphrase</source>
+        <translation>암호구절을 사용하여 입력 한 키 암호 해독</translation>
+    </message>
+    <message>
+        <source>Encrypt &amp;Key</source>
+        <translation>암호화 &amp; 키</translation>
+    </message>
+    <message>
+        <source>Clear &amp;All</source>
+        <translation>전체 &amp;지우기</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Decrypt</source>
+        <translation>&amp;BIP 38 암호화 해제</translation>
+    </message>
+    <message>
+        <source>Enter the BIP 38 encrypted private key. Enter the passphrase in the middle box. Click Decrypt Key to compute the private key. After the key is decrypted, clicking 'Import Address' will add this private key to the wallet.</source>
+        <translation>BIP 38 암호화 된 개인 키를 입력하십시오. 중간 입력창에 암호를 입력하십시오. 키를 암호해제 후 '주소 가져오기'를 클릭하면 개인 키가 지갑에 추가됩니다.</translation>
+    </message>
+    <message>
+        <source>Decrypt &amp;Key</source>
+        <translation>암호해제 &amp;키</translation>
+    </message>
+    <message>
+        <source>Decrypted Key:</source>
+        <translation>암호해제된 키</translation>
+    </message>
+    <message>
+        <source>Import Address</source>
+        <translation>주소 가져오기</translation>
+    </message>
+    <message>
+        <source>Click "Decrypt Key" to compute key</source>
+        <translation>"키 암호해제"를 클릭하여 키를 입력해주세요.</translation>
+    </message>
+    <message>
+        <source>The entered passphrase is invalid. </source>
+        <translation>입력한 암호 문구가 잘못되었습니다.</translation>
+    </message>
+    <message>
+        <source>Allowed: 0-9,a-z,A-Z,</source>
+        <translation>사용가능: 0-9,a-z,A-Z,</translation>
+    </message>
+    <message>
+        <source>The entered address is invalid.</source>
+        <translation>입력한 주소가 올바르지 않습니다.</translation>
+    </message>
+    <message>
+        <source>Please check the address and try again.</source>
+        <translation>주소를 다시 확인해주세요.</translation>
+    </message>
+    <message>
+        <source>The entered address does not refer to a key.</source>
+        <translation>입력한 주소가 키를 참조하지 않습니다.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock was cancelled.</source>
+        <translation>지갑 잠금 해제가 취소되었습니다.</translation>
+    </message>
+    <message>
+        <source>Private key for the entered address is not available.</source>
+        <translation>입력한 주소의 개인 키를 사용할 수 없습니다.</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt.</source>
+        <translation>암호해제에 실패하였습니다.</translation>
+    </message>
+    <message>
+        <source>Please check the key and passphrase and try again.</source>
+        <translation>키와 암호를 확인하고 다시 시도해주세요.</translation>
+    </message>
+    <message>
+        <source>Data Not Valid.</source>
+        <translation>데이터가 올바르지 않습니다.</translation>
+    </message>
+    <message>
+        <source>Please try again.</source>
+        <translation>다시 시도해주세요.</translation>
+    </message>
+    <message>
+        <source>Please wait while key is imported</source>
+        <translation>키를 가져오는 동안 기다려주세요.</translation>
+    </message>
+    <message>
+        <source>Key Already Held By Wallet</source>
+        <translation>이미 지갑에서 사용된 키</translation>
+    </message>
+    <message>
+        <source>Error Adding Key To Wallet</source>
+        <translation>지갑에 키를 추가하는 도중 에러가 발생했습니다</translation>
+    </message>
+    <message>
+        <source>Successfully Added Private Key To Wallet</source>
+        <translation>지갑에 개인 키를 추가하였습니다.</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Wallet</source>
+        <translation>지갑</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>노드</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>&amp;개요</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>지갑의 일반적인 개요 표시</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>&amp;보내기</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>&amp;받기</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>&amp;거래내역</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>겨래내역 보기</translation>
+    </message>
+    <message>
+        <source>Privacy Actions for zGIC</source>
+        <translation>zGIC 생성 및 관리</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>종&amp;료</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>어플리케이션 종료</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>&amp;Qt 정보</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Qt에 대한 정보 표시</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>&amp;옵션...</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>&amp;보이기 / 숨기기</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>기본 창 보이기 또는 숨기기</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>&amp;지갑 암호화...</translation>
+    </message>
+    <message>
+        <source>Encrypt the private keys that belong to your wallet</source>
+        <translation>지갑 개인 키 암호화</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>&amp;지갑 백업</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>다른 위치에 지갑 백업</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>&amp;암호 변경...</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>지갑 암호화에 사용되는 암호 변경</translation>
+    </message>
+    <message>
+        <source>&amp;Unlock Wallet...</source>
+        <translatio
