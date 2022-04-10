c1160654d99beb076ed0793367a69b2ab0dd1c9e
3q5y <107141565+3q5y@users.noreply.github.com>
2022-04-10 18:47:36 +0000

    Enable travis-ci

--- src/qt/locale/giant_lt_LT.ts
+++ src/qt/locale/giant_lt_LT.ts
@@ -7,4 +7,237 @@
     </message>
     <message>
         <source>Create a new address</source>
-        <tran
+        <translation>Sukurti naują adresą</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>&amp;Naujas</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Kopijuoti pažymėtą adresą</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>&amp;Kopijuoti</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Ištrinti dabartinį pažymėtą adresą iš sąrašo</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>&amp;Ištrinti</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Eksportuoti duomenis dabartiniame skirtuke į failą</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>&amp;Eksportuoti</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>&amp;Uždaryti</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Pasirinkite adresą, kuriam siųsite monetas</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Pasirinkite adresą, kuriuo gausite monetas</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>&amp;Pasirinkti</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Siuntimo adresai</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Gavimo adresai</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Tai jūsų GIANT adresai mokėjimų išsiuntimams. Visada patikrinkite sumą ir gavėjo adresą prieš siųsdami monetas.</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Tai jūsų GIANT adresai mokėjimų gavimams. Rekomenduojama naudoti naują adresą kiekvienam pavedimui.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>&amp;Kopijuoti Adresą</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Kopijuoti &amp;Etiketė</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>&amp;Redaguoti</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Eksportuoti adresų sąrašą</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Taškais atskirtas failas (* .csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Eksportavimas Nepavyko</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Įvyko klaida bandant išsaugoti adresų sąrašą į %1. Bandykite dar kartą.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Etiketė</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresas</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(nėra etiketės)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Slaptafrazės dialogas</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Įveskite slaptafrazę</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nauja slaptafrazė</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Pakartokite naują slaptafrazę</translation>
+    </message>
+    <message>
+        <source>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</source>
+        <translation>Pašalina nereikšmingą sendmoney, kai OS sąskaita pažeista. Nesuteikia jokio realaus saugumo.</translation>
+    </message>
+    <message>
+        <source>For anonymization, automint, and staking only</source>
+        <translation>Tik dėl anonimizacijos ir "kasimo"</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Suveskite naują slaptafrazę į piniginę, &lt;br/&gt;Prašome naudoti frazę sudarytą iš &lt;b&gt; dešimt ar daugiau atsitiktinių simbolių&lt;/b&gt;, arba &lt;b&gt;aštuonis ir daugiau žodžių&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Užšifruoti piniginę</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Ši operacija reikalauja jūsų piniginės slaptafrazės, tam, kad ją atrakintumėte.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Atrakinti piniginę</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Ši operacija reikalauja jūsų piniginės slaptafrazės, tam, kad ją iššifruotumėte.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Iššifruoti piniginę</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Pakeisti slaptafrazę</translation>
+    </message>
+    <message>
+        <source>Enter the old and new passphrase to the wallet.</source>
+        <translation>Įveskite seną ir naują slaptafrazę į piniginę.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Patvirtinkite piniginės užšifravimą.</translation>
+    </message>
+    <message>
+        <source>GIANT will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your GICs from being stolen by malware infecting your computer.</source>
+        <translation>Dabar GIANT išsijungs, kad būtų užbaigtas šifravimo procesas. Atminkite, kad jūsų piniginės užšifravimas negali visiškai apsaugoti jūsų GIC nuo kenkėjiškų programų, užkrėsto jūsų kompiuterio.</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Ar tikrai norite užšifruoti savo piniginę?</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GIC&lt;/b&gt;!</source>
+        <translation>Įspėjimas: jei užšifruosite savo piniginę ir prarasite slaptafrazę, jūs &lt;b&gt;PRARASITE VISUS SAVO GIC&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Piniginė užšifruota</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>SVARBU: bet kokios ankstesnės atsarginės piniginės kopijos, turėtų būti pakeistos naujai sukurtu, užšifruotu piniginės failu. Saugumo sumetimais ankstesnės neužšifruoto piniginės failo atsarginės kopijos bus netinkamos, kai tik pradėsite naudoti naują užšifruotą piniginę.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Piniginės užšifravimas nepavyko</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Piniginės užšifravimas nepavyko dėl vidinės klaidos. Jūsų piniginė nebuvo užšifruota</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Pateiktos slaptafrazės neatitinka.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Piniginės atrakinimas nepavyko.</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Įvesta slaptafrazė piniginės iššifravimui yra neteisinga.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Piniginės iššifravimas nepavyko</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Piniginės slaptafrazė sėkmingai pakeista.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Įspėjimas: mygtukas "Caps Lock" yra įjungtas!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    <message>
+        <source>IP/Netmask</source>
+        <translation>IP/Netmask</translation>
+    </message>
+    <message>
+        <source>Banned Until</source>
+        <translation>Uždraustas Iki</translation>
+    </message>
+</context>
+<context>
+    <name>Bip38To
