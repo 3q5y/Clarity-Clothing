ba07d010cb18efe5f3b21f28b998bee27216c268
3q5y <107141565+3q5y@users.noreply.github.com>
2022-05-21 21:36:41 +0000

    small code improvement

--- src/qt/locale/giant_sk.ts
+++ src/qt/locale/giant_sk.ts
@@ -1,1 +1,234 @@
-<TS language="sk" version="2.1
+<TS language="sk" version="2.1">
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <source>Right-click to edit address or label</source>
+        <translation>Pravým tlačidlom myši upravte adresu alebo štítok</translation>
+    </message>
+    <message>
+        <source>Create a new address</source>
+        <translation>Vytvoriť novú adresu</translation>
+    </message>
+    <message>
+        <source>&amp;New</source>
+        <translation>Nový</translation>
+    </message>
+    <message>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Skopírovať označenú adresu do schránky</translation>
+    </message>
+    <message>
+        <source>&amp;Copy</source>
+        <translation>Kopírovať</translation>
+    </message>
+    <message>
+        <source>Delete the currently selected address from the list</source>
+        <translation>Vymazať označenú adresu zo zoznamu</translation>
+    </message>
+    <message>
+        <source>&amp;Delete</source>
+        <translation>Vymazať</translation>
+    </message>
+    <message>
+        <source>Export the data in the current tab to a file</source>
+        <translation>Exportovať údaje na aktuálnej karte do súboru</translation>
+    </message>
+    <message>
+        <source>&amp;Export</source>
+        <translation>Exportovať</translation>
+    </message>
+    <message>
+        <source>C&amp;lose</source>
+        <translation>Zatvoriť</translation>
+    </message>
+    <message>
+        <source>Choose the address to send coins to</source>
+        <translation>Vyberte adresu, na ktorú chcete odoslať mince</translation>
+    </message>
+    <message>
+        <source>Choose the address to receive coins with</source>
+        <translation>Vyberte adresu, na ktorej chcete mince prijať</translation>
+    </message>
+    <message>
+        <source>C&amp;hoose</source>
+        <translation>Vybrať</translation>
+    </message>
+    <message>
+        <source>Sending addresses</source>
+        <translation>Odosielacia adresa</translation>
+    </message>
+    <message>
+        <source>Receiving addresses</source>
+        <translation>Prijímacia adresa</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for sending payments. Always check the amount and the receiving address before sending coins.</source>
+        <translation>Toto sú vaše GIANT adresy na odosielanie platieb. Pred odoslaním vždy skontrolujte výšku a adresu prijemcu.</translation>
+    </message>
+    <message>
+        <source>These are your GIANT addresses for receiving payments. It is recommended to use a new receiving address for each transaction.</source>
+        <translation>Toto sú vaše GIANT adresy na prijímanie platieb. Pre každú transakciu sa odporúča použiť novú prijímaciu adresu.</translation>
+    </message>
+    <message>
+        <source>&amp;Copy Address</source>
+        <translation>Skopírovať adresu</translation>
+    </message>
+    <message>
+        <source>Copy &amp;Label</source>
+        <translation>Skopírovať štítok</translation>
+    </message>
+    <message>
+        <source>&amp;Edit</source>
+        <translation>Editovať</translation>
+    </message>
+    <message>
+        <source>Export Address List</source>
+        <translation>Exportovať zoznamu adries</translation>
+    </message>
+    <message>
+        <source>Comma separated file (*.csv)</source>
+        <translation>Súbor oddelený čiarkami (*.csv)</translation>
+    </message>
+    <message>
+        <source>Exporting Failed</source>
+        <translation>Export zlyhal</translation>
+    </message>
+    <message>
+        <source>There was an error trying to save the address list to %1. Please try again.</source>
+        <translation>Pri pokuse o uloženie zoznamu adries na %1 sa vyskytla chyba. Prosím skúste znova.</translation>
+    </message>
+</context>
+<context>
+    <name>AddressTableModel</name>
+    <message>
+        <source>Label</source>
+        <translation>Štítok</translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <translation>Adresa</translation>
+    </message>
+    <message>
+        <source>(no label)</source>
+        <translation>(žiaden štítok)</translation>
+    </message>
+</context>
+<context>
+    <name>AskPassphraseDialog</name>
+    <message>
+        <source>Passphrase Dialog</source>
+        <translation>Okno prístupovej frázy</translation>
+    </message>
+    <message>
+        <source>Enter passphrase</source>
+        <translation>Zadajte prístupovú frázu</translation>
+    </message>
+    <message>
+        <source>New passphrase</source>
+        <translation>Nová prístupová fráza</translation>
+    </message>
+    <message>
+        <source>Repeat new passphrase</source>
+        <translation>Zopakujte prístupovú frázu</translation>
+    </message>
+    <message>
+        <source>Serves to disable the trivial sendmoney when OS account compromised. Provides no real security.</source>
+        <translation>Slúži na zakázanie obyčajného odoslania peňazí, v prípade, že bol OS účet kompromitovaný. Neposkytuje žiadnu skutočnú bezpečnosť.</translation>
+    </message>
+    <message>
+        <source>Enter the new passphrase to the wallet.&lt;br/&gt;Please use a passphrase of &lt;b&gt;ten or more random characters&lt;/b&gt;, or &lt;b&gt;eight or more words&lt;/b&gt;.</source>
+        <translation>Zadajte novú prístupovú frázu do peňaženky. &lt;br/&gt; Použite prístupovú frázu &lt;b&gt;, ktorá má desať alebo viac náhodných znakov &lt;/ b&gt; alebo &lt;b&gt; osem alebo viac slov &lt;/ b&gt;.</translation>
+    </message>
+    <message>
+        <source>Encrypt wallet</source>
+        <translation>Šifrovať peňaženku</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to unlock the wallet.</source>
+        <translation>Táto operácia potrebuje prístupovú frázu peňaženky na odomknutie peňaženky.</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Odomknúť peňaženku</translation>
+    </message>
+    <message>
+        <source>This operation needs your wallet passphrase to decrypt the wallet.</source>
+        <translation>Táto operácia potrebuje prístupovú frázu peňaženky na dešifrovanie peňaženky.</translation>
+    </message>
+    <message>
+        <source>Decrypt wallet</source>
+        <translation>Dešifrovať peňaženku</translation>
+    </message>
+    <message>
+        <source>Change passphrase</source>
+        <translation>Zmeniť prístupovú frázu</translation>
+    </message>
+    <message>
+        <source>Enter the old and new passphrase to the wallet.</source>
+        <translation>Zadajte starú a novú prístupovú frázu do peňaženky.</translation>
+    </message>
+    <message>
+        <source>Confirm wallet encryption</source>
+        <translation>Potvrďte šifrovanie peňaženky</translation>
+    </message>
+    <message>
+        <source>GIANT will close now to finish the encryption process. Remember that encrypting your wallet cannot fully protect your GICs from being stolen by malware infecting your computer.</source>
+        <translation>GIANT sa teraz ukončí, aby dokončil proces šifrovania. Nezabudnite, že šifrovanie peňaženky nemôže úplne ochrániť vaše GIC pred odcudzením škodlivým softvérom, ktorý infikuje váš počítač.</translation>
+    </message>
+    <message>
+        <source>Are you sure you wish to encrypt your wallet?</source>
+        <translation>Naozaj chcete zašifrovať svoju peňaženku?</translation>
+    </message>
+    <message>
+        <source>Warning: If you encrypt your wallet and lose your passphrase, you will &lt;b&gt;LOSE ALL OF YOUR GIC&lt;/b&gt;!</source>
+        <translation>Upozornenie: Ak zašifrujete svoju peňaženku a stratíte svoju prístupovú frázu, &lt;b&gt; PRÍDETE O VŠETKY GIC &lt;/ b&gt;!</translation>
+    </message>
+    <message>
+        <source>Wallet encrypted</source>
+        <translation>Peňaženka zašifrovaná</translation>
+    </message>
+    <message>
+        <source>IMPORTANT: Any previous backups you have made of your wallet file should be replaced with the newly generated, encrypted wallet file. For security reasons, previous backups of the unencrypted wallet file will become useless as soon as you start using the new, encrypted wallet.</source>
+        <translation>DÔLEŽITÉ: Všetky predchádzajúce zálohy súboru peňaženky, by mali byť nahradené novo vytvoreným šifrovaným súborom peňaženky. Z bezpečnostných dôvodov sa predchádzajúce zálohy súboru nezašifrovanej peňaženky stanú nepotrebné, akonáhle začnete používať novú zašifrovanú peňaženku.</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed</source>
+        <translation>Šifrovanie peňaženky zlyhalo</translation>
+    </message>
+    <message>
+        <source>Wallet encryption failed due to an internal error. Your wallet was not encrypted.</source>
+        <translation>Šifrovanie peňaženky zlyhalo kvôli internej chybe. Vaša peňaženka nebola zašifrovaná.</translation>
+    </message>
+    <message>
+        <source>The supplied passphrases do not match.</source>
+        <translation>Zadané prístupové frázy sa nezhodujú.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock failed</source>
+        <translation>Odomknutie peňaženky zlyhalo</translation>
+    </message>
+    <message>
+        <source>The passphrase entered for the wallet decryption was incorrect.</source>
+        <translation>Zadaná prístupová fráza pre dešifrovanie peňaženky bola nesprávna.</translation>
+    </message>
+    <message>
+        <source>Wallet decryption failed</source>
+        <translation>Dešifrovanie peňaženky zlyhalo</translation>
+    </message>
+    <message>
+        <source>Wallet passphrase was successfully changed.</source>
+        <translation>Prístupová fráza pre peňaženku bola úspešne zmenená.</translation>
+    </message>
+    <message>
+        <source>Warning: The Caps Lock key is on!</source>
+        <translation>Upozornenie: Tlačidlo Caps Lock je zapnuté!</translation>
+    </message>
+</context>
+<context>
+    <name>BanTableModel</name>
+    </context>
+<context>
+    <name>Bip38ToolDialog</name>
+    <message>
+        <source>BIP 38 Tool</source>
+        <translation>BIP 38 Utili
