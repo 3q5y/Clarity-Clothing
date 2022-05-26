1ace5479646f94a87d61966655bcf046cd2671e4
3q5y <107141565+3q5y@users.noreply.github.com>
2022-05-26 11:18:06 +0000

    spring boot

--- src/qt/locale/giant_sk.ts
+++ src/qt/locale/giant_sk.ts
@@ -231,4 +231,283 @@
     <name>Bip38ToolDialog</name>
     <message>
         <source>BIP 38 Tool</source>
-        <translation>BIP 38 Utili
+        <translation>BIP 38 Utilita</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Encrypt</source>
+        <translation>BIP 38 Šifrovanie</translation>
+    </message>
+    <message>
+        <source>Address:</source>
+        <translation>Adresa:</translation>
+    </message>
+    <message>
+        <source>Choose previously used address</source>
+        <translation>Vyberte predtým použitú adresu</translation>
+    </message>
+    <message>
+        <source>Alt+A</source>
+        <translation>Alt+A</translation>
+    </message>
+    <message>
+        <source>Paste address from clipboard</source>
+        <translation>Vložte adresu zo schránky</translation>
+    </message>
+    <message>
+        <source>Alt+P</source>
+        <translation>Alt+P</translation>
+    </message>
+    <message>
+        <source>Passphrase: </source>
+        <translation>Prístupová fráza:</translation>
+    </message>
+    <message>
+        <source>Encrypted Key:</source>
+        <translation>Šifrovaný kľúč:</translation>
+    </message>
+    <message>
+        <source>Copy the current signature to the system clipboard</source>
+        <translation>Skopírujte aktuálny podpis do schránky</translation>
+    </message>
+    <message>
+        <source>Encrypt &amp;Key</source>
+        <translation>Zašifrovať kľúč</translation>
+    </message>
+    <message>
+        <source>Clear &amp;All</source>
+        <translation>Všetko zmazať</translation>
+    </message>
+    <message>
+        <source>&amp;BIP 38 Decrypt</source>
+        <translation>BIP 38 dešifrovanie</translation>
+    </message>
+    <message>
+        <source>Enter the BIP 38 encrypted private key. Enter the passphrase in the middle box. Click Decrypt Key to compute the private key. After the key is decrypted, clicking 'Import Address' will add this private key to the wallet.</source>
+        <translation>Zadajte súkromný kľúč zašifrovaný BIP 38. Zadajte prístupovú frázu do stredného poľa. Kliknite na Dešifrovať kľúč, ak chcete vypočítať privátny kľúč. Po dešifrovaní kľúča pridáte do peňaženky tento súkromný kľúč kliknutím na položku 'Importovať adresu'.</translation>
+    </message>
+    <message>
+        <source>Decrypt &amp;Key</source>
+        <translation>Dešifrovať kľúč</translation>
+    </message>
+    <message>
+        <source>Decrypted Key:</source>
+        <translation>Dešifrovaný kľúč:</translation>
+    </message>
+    <message>
+        <source>Import Address</source>
+        <translation>Importovať adresu:</translation>
+    </message>
+    <message>
+        <source>Click "Decrypt Key" to compute key</source>
+        <translation>Kliknite na tlačidlo "Dešifrovať kľúč" pre výpočet kľúča</translation>
+    </message>
+    <message>
+        <source>The entered passphrase is invalid. </source>
+        <translation>Zadaná prístupová fráza je neplatná.</translation>
+    </message>
+    <message>
+        <source>Allowed: 0-9,a-z,A-Z,</source>
+        <translation>Povolené: 0-9, a-z, A-Z,</translation>
+    </message>
+    <message>
+        <source>The entered address is invalid.</source>
+        <translation>Zadaná adresa je neplatná.</translation>
+    </message>
+    <message>
+        <source>Please check the address and try again.</source>
+        <translation>Skontrolujte adresu a skúste to znova.</translation>
+    </message>
+    <message>
+        <source>The entered address does not refer to a key.</source>
+        <translation>Zadaná adresa neodkazuje na kľúč.</translation>
+    </message>
+    <message>
+        <source>Wallet unlock was cancelled.</source>
+        <translation>Odomknutie peňaženky bolo zrušené.</translation>
+    </message>
+    <message>
+        <source>Private key for the entered address is not available.</source>
+        <translation>Súkromný kľúč pre zadanú adresu nie je k dispozícii.</translation>
+    </message>
+    <message>
+        <source>Failed to decrypt.</source>
+        <translation>Dešifrovanie zlyhalo.</translation>
+    </message>
+    <message>
+        <source>Please check the key and passphrase and try again.</source>
+        <translation>Skontrolujte, prosím, kľúč a prístupovú frázu a skúste to znova.</translation>
+    </message>
+    <message>
+        <source>Data Not Valid.</source>
+        <translation>Neplatné dáta.</translation>
+    </message>
+    <message>
+        <source>Please try again.</source>
+        <translation>Prosím skúste znova.</translation>
+    </message>
+    <message>
+        <source>Please wait while key is imported</source>
+        <translation>Počkajte, kým sa importuje kľúč</translation>
+    </message>
+    <message>
+        <source>Key Already Held By Wallet</source>
+        <translation>Kľúč je už uložený v peňaženke</translation>
+    </message>
+    <message>
+        <source>Error Adding Key To Wallet</source>
+        <translation>Chyba pri pridávaní kľúča do peňaženky</translation>
+    </message>
+    <message>
+        <source>Successfully Added Private Key To Wallet</source>
+        <translation>Súkromný kľúč bol úspešne pridaný do peňaženky</translation>
+    </message>
+</context>
+<context>
+    <name>BitcoinGUI</name>
+    <message>
+        <source>Wallet</source>
+        <translation>Peňaženka</translation>
+    </message>
+    <message>
+        <source>Node</source>
+        <translation>Uzol</translation>
+    </message>
+    <message>
+        <source>&amp;Overview</source>
+        <translation>Prehľad</translation>
+    </message>
+    <message>
+        <source>Show general overview of wallet</source>
+        <translation>Zobraziť všeobecný prehľad peňaženky</translation>
+    </message>
+    <message>
+        <source>&amp;Send</source>
+        <translation>Odoslať</translation>
+    </message>
+    <message>
+        <source>&amp;Receive</source>
+        <translation>Prijať</translation>
+    </message>
+    <message>
+        <source>&amp;Transactions</source>
+        <translation>Transakcie</translation>
+    </message>
+    <message>
+        <source>Browse transaction history</source>
+        <translation>Prezrieť históriu transakcií</translation>
+    </message>
+    <message>
+        <source>E&amp;xit</source>
+        <translation>Koniec</translation>
+    </message>
+    <message>
+        <source>Quit application</source>
+        <translation>Ukončiť aplikáciu</translation>
+    </message>
+    <message>
+        <source>About &amp;Qt</source>
+        <translation>O aplikácii</translation>
+    </message>
+    <message>
+        <source>Show information about Qt</source>
+        <translation>Zobraziť informácie o Qt</translation>
+    </message>
+    <message>
+        <source>&amp;Options...</source>
+        <translation>Možnosti...</translation>
+    </message>
+    <message>
+        <source>&amp;Show / Hide</source>
+        <translation>Zobraziť / Skryť</translation>
+    </message>
+    <message>
+        <source>Show or hide the main Window</source>
+        <translation>Zobraziť alebo skryť hlavné okno</translation>
+    </message>
+    <message>
+        <source>&amp;Encrypt Wallet...</source>
+        <translation>Šifrovať peňaženku...</translation>
+    </message>
+    <message>
+        <source>Encrypt the private keys that belong to your wallet</source>
+        <translation>Šifrujte súkromné kľúče, ktoré patria Vašej peňaženke</translation>
+    </message>
+    <message>
+        <source>&amp;Backup Wallet...</source>
+        <translation>Zálohovať peňaženku...</translation>
+    </message>
+    <message>
+        <source>Backup wallet to another location</source>
+        <translation>Zálohovať peňaženku na iné miesto</translation>
+    </message>
+    <message>
+        <source>&amp;Change Passphrase...</source>
+        <translation>Zmeniť prístupovú frázu...</translation>
+    </message>
+    <message>
+        <source>Change the passphrase used for wallet encryption</source>
+        <translation>Zmeniť prístupovú frázu, ktorá bola použitá na šifrovanie peňaženky</translation>
+    </message>
+    <message>
+        <source>&amp;Unlock Wallet...</source>
+        <translation>Odomknúť peňaženku...</translation>
+    </message>
+    <message>
+        <source>Unlock wallet</source>
+        <translation>Odomknúť peňaženku</translation>
+    </message>
+    <message>
+        <source>&amp;Lock Wallet</source>
+        <translation>Zamknúť peňaženku</translation>
+    </message>
+    <message>
+        <source>Sign &amp;message...</source>
+        <translation>Podpísať správu...</translation>
+    </message>
+    <message>
+        <source>&amp;Verify message...</source>
+        <translation>Overiť správu...</translation>
+    </message>
+    <message>
+        <source>&amp;Information</source>
+        <translation>Informácie</translation>
+    </message>
+    <message>
+        <source>Show diagnostic information</source>
+        <translation>Zobraziť diagnostické informácie</translation>
+    </message>
+    <message>
+        <source>&amp;Debug console</source>
+        <translation>Debugovacia konzola</translation>
+    </message>
+    <message>
+        <source>Open debugging console</source>
+        <translation>Otvoriť debugovaciu konzola</translation>
+    </message>
+    <message>
+        <source>&amp;Network Monitor</source>
+        <translation>Monitor siete</translation>
+    </message>
+    <message>
+        <source>Show network monitor</source>
+        <translation>Zobraziť monitor siete</translation>
+    </message>
+    <message>
+        <source>&amp;Peers list</source>
+        <translation>Zoznam uzlov</translation>
+    </message>
+    <message>
+        <source>Show peers info</source>
+        <translation>Zobraziť informácie o uzloch</translation>
+    </message>
+    <message>
+        <source>Wallet &amp;Repair</source>
+        <translation>Oprava peňaženky</translation>
+    </message>
+    <message>
+        <source>Show wallet repair options</source>
+        <translation>Zobraziť možnosti opravy peňaženky</translation>
+    </message>
+    <message>
+        <source>Open configuration file</source>
+    
