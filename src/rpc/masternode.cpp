6ac2322824c9970f7b892e53390c1653077fe353
3q5y <107141565+3q5y@users.noreply.github.com>
2022-07-10 18:52:30 +0000

    new names

--- src/rpc/masternode.cpp
+++ src/rpc/masternode.cpp
@@ -235,4 +235,246 @@ UniValue getmasternodecount (const UniValue& params, bool fHelp)
             "}\n"
 
             "\nExamples:\n" +
-            HelpExampleCli("getmasternodecount", "") + HelpExampleRpc("
+            HelpExampleCli("getmasternodecount", "") + HelpExampleRpc("getmasternodecount", ""));
+
+
+    UniValue total{UniValue::VARR};
+    UniValue stable{UniValue::VARR};
+    UniValue obfcompat{UniValue::VARR};
+    UniValue enabled{UniValue::VARR};
+    UniValue inqueue{UniValue::VARR};
+    UniValue ipv4{UniValue::VARR};
+    UniValue ipv6{UniValue::VARR};
+    UniValue onion{UniValue::VARR};
+
+    auto chainTip = chainActive.Tip();
+
+    for(int l = CMasternode::begin(chainActive.Height()); l <= CMasternode::end(chainActive.Height()); l++) {
+
+        std::string levelText = CMasternode::GetLevelText(l);
+
+        UniValue totalByLevel{UniValue::VOBJ};
+        totalByLevel.push_back(Pair("level", levelText));
+        totalByLevel.push_back(Pair("count", mnodeman.size(l)));
+        total.push_back(totalByLevel);
+
+        UniValue stableByLevel{UniValue::VOBJ};
+        stableByLevel.push_back(Pair("level", levelText));
+        stableByLevel.push_back(Pair("count", mnodeman.stable_size(l)));
+        stable.push_back(stableByLevel);
+
+        UniValue enabledByLevel{UniValue::VOBJ};
+        enabledByLevel.push_back(Pair("level", levelText));
+        enabledByLevel.push_back(Pair("count", mnodeman.CountEnabled(l)));
+        enabled.push_back(enabledByLevel);
+
+        UniValue inqueueByLevel{UniValue::VOBJ};
+        int inqueueCount = 0u;
+        if(chainTip) {
+            mnodeman.GetNextMasternodeInQueueForPayment(chainTip->nHeight, l, true, inqueueCount);
+        }
+        inqueueByLevel.push_back(Pair("level", levelText));
+        inqueueByLevel.push_back(Pair("count", inqueueCount));
+
+        inqueue.push_back(inqueueByLevel);
+
+        UniValue obfcomatByLevel{UniValue::VOBJ};
+        obfcomatByLevel.push_back(Pair("level", levelText));
+        obfcomatByLevel.push_back(Pair("count", mnodeman.CountEnabled(l, ActiveProtocol())));
+        obfcompat.push_back(obfcomatByLevel);
+
+        int ipv4Count = 0;
+        int ipv6Count = 0;
+        int onionCount = 0;
+        mnodeman.CountNetworks(ActiveProtocol(), ipv4Count, ipv6Count, onionCount, l);
+
+        UniValue ipv4ByLevel{UniValue::VOBJ};
+        ipv4ByLevel.push_back(Pair("level", levelText));
+        ipv4ByLevel.push_back(Pair("count", ipv4Count));
+        ipv4.push_back(ipv4ByLevel);
+
+        UniValue ipv6ByLevel{UniValue::VOBJ};
+        ipv6ByLevel.push_back(Pair("level", levelText));
+        ipv6ByLevel.push_back(Pair("count", ipv6Count));
+        ipv6.push_back(ipv6ByLevel);
+
+        UniValue onionByLevel{UniValue::VOBJ};
+        onionByLevel.push_back(Pair("level", levelText));
+        onionByLevel.push_back(Pair("count", onionCount));
+        onion.push_back(onionByLevel);
+    }
+
+    UniValue obj(UniValue::VOBJ);
+    obj.push_back(Pair("all", mnodeman.size()));
+    obj.push_back(Pair("total", total));
+    obj.push_back(Pair("enabled", enabled));
+    obj.push_back(Pair("obfcompat", obfcompat));
+    obj.push_back(Pair("stable", stable));
+    obj.push_back(Pair("inqueue", inqueue));
+    obj.push_back(Pair("ipv4", ipv4));
+    obj.push_back(Pair("ipv6", ipv6));
+    obj.push_back(Pair("onion", onion));
+    return obj;
+}
+
+UniValue masternodecurrent (const UniValue& params, bool fHelp)
+{
+    if (fHelp || (params.size() != 0))
+        throw runtime_error(
+            "masternodecurrent\n"
+            "\nGet current masternode winner\n"
+
+            "\nResult:\n"
+            "{\n"
+            "  \"level\": xxxx,         (numeric) MN level\n"
+            "  \"protocol\": xxxx,      (numeric) Protocol version\n"
+            "  \"txhash\": \"xxxx\",    (string) Collateral transaction hash\n"
+            "  \"pubkey\": \"xxxx\",    (string) MN Public key\n"
+            "  \"lastseen\": xxx,       (numeric) Time since epoch of last seen\n"
+            "  \"activeseconds\": xxx,  (numeric) Seconds MN has been active\n"
+            "}\n"
+
+            "\nExamples:\n" +
+            HelpExampleCli("masternodecurrent", "") + HelpExampleRpc("masternodecurrent", ""));
+
+    UniValue result{UniValue::VARR};
+    for(int l = CMasternode::begin(chainActive.Height()); l <= CMasternode::end(chainActive.Height()); l++) {
+        CMasternode* winner = mnodeman.GetCurrentMasterNode(l, 1);
+        if(!winner)
+            continue;
+
+        UniValue obj(UniValue::VOBJ);
+        obj.push_back(Pair("level", winner->GetLevelText()));
+        obj.push_back(Pair("protocol", (int64_t)winner->protocolVersion));
+        obj.push_back(Pair("txhash", winner->vin.prevout.hash.ToString()));
+        obj.push_back(Pair("outputidx", (uint64_t)winner->vin.prevout.n));
+        obj.push_back(Pair("pubkey", CBitcoinAddress(winner->pubKeyCollateralAddress.GetID()).ToString()));
+        obj.push_back(Pair("lastseen", (winner->lastPing == CMasternodePing()) ? winner->sigTime : (int64_t)winner->lastPing.sigTime));
+        obj.push_back(Pair("activeseconds", (winner->lastPing == CMasternodePing()) ? 0 : (int64_t)(winner->lastPing.sigTime - winner->sigTime)));
+        result.push_back(obj);
+    }
+    return result;
+}
+
+UniValue masternodedebug (const UniValue& params, bool fHelp)
+{
+    if (fHelp || (params.size() != 0))
+        throw runtime_error(
+            "masternodedebug\n"
+            "\nPrint masternode status\n"
+
+            "\nResult:\n"
+            "\"status\"     (string) Masternode status message\n"
+
+            "\nExamples:\n" +
+            HelpExampleCli("masternodedebug", "") + HelpExampleRpc("masternodedebug", ""));
+
+    if (activeMasternode.status != ACTIVE_MASTERNODE_INITIAL || !masternodeSync.IsSynced())
+        return activeMasternode.GetStatus();
+
+    CTxIn vin = CTxIn();
+    CPubKey pubkey;
+    CKey key;
+    if (!activeMasternode.GetMasterNodeVin(vin, pubkey, key))
+        throw runtime_error("Missing masternode input, please look at the documentation for instructions on masternode creation\n");
+    else
+        return activeMasternode.GetStatus();
+}
+
+UniValue startmasternode (const UniValue& params, bool fHelp)
+{
+    std::string strCommand;
+    if (params.size() >= 1) {
+        strCommand = params[0].get_str();
+
+        // Backwards compatibility with legacy 'masternode' super-command forwarder
+        if (strCommand == "start") strCommand = "local";
+        if (strCommand == "start-alias") strCommand = "alias";
+        if (strCommand == "start-all") strCommand = "all";
+        if (strCommand == "start-many") strCommand = "many";
+        if (strCommand == "start-missing") strCommand = "missing";
+        if (strCommand == "start-disabled") strCommand = "disabled";
+    }
+
+    if (fHelp || params.size() < 2 || params.size() > 3 ||
+        (params.size() == 2 && (strCommand != "local" && strCommand != "all" && strCommand != "many" && strCommand != "missing" && strCommand != "disabled")) ||
+        (params.size() == 3 && strCommand != "alias"))
+        throw runtime_error(
+            "startmasternode \"local|all|many|missing|disabled|alias\" lockwallet ( \"alias\" )\n"
+            "\nAttempts to start one or more masternode(s)\n"
+
+            "\nArguments:\n"
+            "1. set         (string, required) Specify which set of masternode(s) to start.\n"
+            "2. lockwallet  (boolean, required) Lock wallet after completion.\n"
+            "3. alias       (string) Masternode alias. Required if using 'alias' as the set.\n"
+
+            "\nResult: (for 'local' set):\n"
+            "\"status\"     (string) Masternode status message\n"
+
+            "\nResult: (for other sets):\n"
+            "{\n"
+            "  \"overall\": \"xxxx\",     (string) Overall status message\n"
+            "  \"detail\": [\n"
+            "    {\n"
+            "      \"node\": \"xxxx\",    (string) Node name or alias\n"
+            "      \"result\": \"xxxx\",  (string) 'success' or 'failed'\n"
+            "      \"error\": \"xxxx\"    (string) Error message, if failed\n"
+            "    }\n"
+            "    ,...\n"
+            "  ]\n"
+            "}\n"
+
+            "\nExamples:\n" +
+            HelpExampleCli("startmasternode", "\"alias\" \"0\" \"my_mn\"") + HelpExampleRpc("startmasternode", "\"alias\" \"0\" \"my_mn\""));
+
+    bool fLock = (params[1].get_str() == "true" ? true : false);
+
+    EnsureWalletIsUnlocked();
+
+    if (strCommand == "local") {
+        if (!fMasterNode) throw runtime_error("you must set masternode=1 in the configuration\n");
+
+        if (activeMasternode.status != ACTIVE_MASTERNODE_STARTED) {
+            activeMasternode.status = ACTIVE_MASTERNODE_INITIAL; // TODO: consider better way
+            activeMasternode.ManageStatus();
+            if (fLock)
+                pwalletMain->Lock();
+        }
+
+        return activeMasternode.GetStatus();
+    }
+
+    if (strCommand == "all" || strCommand == "many" || strCommand == "missing" || strCommand == "disabled") {
+        if ((strCommand == "missing" || strCommand == "disabled") &&
+            (masternodeSync.RequestedMasternodeAssets <= MASTERNODE_SYNC_LIST ||
+                masternodeSync.RequestedMasternodeAssets == MASTERNODE_SYNC_FAILED)) {
+            throw runtime_error("You can't use this command until masternode list is synced\n");
+        }
+
+        std::vector<CMasternodeConfig::CMasternodeEntry> mnEntries;
+        mnEntries = masternodeConfig.getEntries();
+
+        int successful = 0;
+        int failed = 0;
+
+        UniValue resultsObj(UniValue::VARR);
+
+        for (CMasternodeConfig::CMasternodeEntry mne : masternodeConfig.getEntries()) {
+            std::string errorMessage;
+            int nIndex;
+            if(!mne.castOutputIndex(nIndex))
+                continue;
+            CTxIn vin = CTxIn(uint256(mne.getTxHash()), uint32_t(nIndex));
+            CMasternode* pmn = mnodeman.Find(vin);
+            CMasternodeBroadcast mnb;
+
+            if (pmn != NULL) {
+                if (strCommand == "missing") continue;
+                if (strCommand == "disabled" && pmn->IsEnabled()) continue;
+            }
+
+            bool result = activeMasternode.CreateBroadcast(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), errorMessage, mnb);
+
+            UniValue statusObj(UniValue::VOBJ);
+            statusObj.push_back(Pair("alias", mne.getAlias()));
+            statusObj.push_back(Pair("result", result ? "success
