59d588acdc609b7311b96c373bf68eed792578c0
3q5y <107141565+3q5y@users.noreply.github.com>
2022-07-23 21:05:28 +0000

    close file

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -158,4 +158,218 @@ vector<unsigned char> ParseHexV(const UniValue& v, string strName)
     if (v.isStr())
         strHex = v.get_str();
     if (!IsHex(strHex))
-        throw J
+        throw JSONRPCError(RPC_INVALID_PARAMETER, strName + " must be hexadecimal string (not '" + strHex + "')");
+    return ParseHex(strHex);
+}
+vector<unsigned char> ParseHexO(const UniValue& o, string strKey)
+{
+    return ParseHexV(find_value(o, strKey), strKey);
+}
+
+int ParseInt(const UniValue& o, string strKey)
+{
+    const UniValue& v = find_value(o, strKey);
+    if (v.isNum())
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, " + strKey + "is not an int");
+
+    return v.get_int();
+}
+
+bool ParseBool(const UniValue& o, string strKey)
+{
+    const UniValue& v = find_value(o, strKey);
+    if (v.isBool())
+        throw JSONRPCError(RPC_INVALID_PARAMETER, "Invalid parameter, " + strKey + "is not a bool");
+
+    return v.get_bool();
+}
+
+
+/**
+ * Note: This interface may still be subject to change.
+ */
+
+string CRPCTable::help(string strCommand) const
+{
+    string strRet;
+    string category;
+    set<rpcfn_type> setDone;
+    vector<pair<string, const CRPCCommand*> > vCommands;
+
+    for (map<string, const CRPCCommand*>::const_iterator mi = mapCommands.begin(); mi != mapCommands.end(); ++mi)
+        vCommands.push_back(make_pair(mi->second->category + mi->first, mi->second));
+    sort(vCommands.begin(), vCommands.end());
+
+    for (const PAIRTYPE(string, const CRPCCommand*) & command : vCommands) {
+        const CRPCCommand* pcmd = command.second;
+        string strMethod = pcmd->name;
+        // We already filter duplicates, but these deprecated screw up the sort order
+        if (strMethod.find("label") != string::npos)
+            continue;
+        if ((strCommand != "" || pcmd->category == "hidden") && strMethod != strCommand)
+            continue;
+#ifdef ENABLE_WALLET
+        if (pcmd->reqWallet && !pwalletMain)
+            continue;
+#endif
+
+        try {
+            UniValue params;
+            rpcfn_type pfn = pcmd->actor;
+            if (setDone.insert(pfn).second)
+                (*pfn)(params, true);
+        } catch (std::exception& e) {
+            // Help text is returned in an exception
+            string strHelp = string(e.what());
+            if (strCommand == "") {
+                if (strHelp.find('\n') != string::npos)
+                    strHelp = strHelp.substr(0, strHelp.find('\n'));
+
+                if (category != pcmd->category) {
+                    if (!category.empty())
+                        strRet += "\n";
+                    category = pcmd->category;
+                    string firstLetter = category.substr(0, 1);
+                    boost::to_upper(firstLetter);
+                    strRet += "== " + firstLetter + category.substr(1) + " ==\n";
+                }
+            }
+            strRet += strHelp + "\n";
+        }
+    }
+    if (strRet == "")
+        strRet = strprintf("help: unknown command: %s\n", strCommand);
+    strRet = strRet.substr(0, strRet.size() - 1);
+    return strRet;
+}
+
+UniValue help(const UniValue& params, bool fHelp)
+{
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "help ( \"command\" )\n"
+            "\nList all commands, or get help for a specified command.\n"
+            "\nArguments:\n"
+            "1. \"command\"     (string, optional) The command to get help on\n"
+            "\nResult:\n"
+            "\"text\"     (string) The help text\n");
+
+    string strCommand;
+    if (params.size() > 0)
+        strCommand = params[0].get_str();
+
+    return tableRPC.help(strCommand);
+}
+
+
+UniValue stop(const UniValue& params, bool fHelp)
+{
+    // Accept the deprecated and ignored 'detach' boolean argument
+    if (fHelp || params.size() > 1)
+        throw runtime_error(
+            "stop\n"
+            "\nStop GIANT server.");
+    // Event loop will exit after current HTTP requests have been handled, so
+    // this reply will get back to the client.
+    StartShutdown();
+    return "GIANT server stopping";
+}
+
+
+/**
+ * Call Table
+ */
+static const CRPCCommand vRPCCommands[] =
+    {
+        //  category              name                      actor (function)         okSafeMode threadSafe reqWallet
+        //  --------------------- ------------------------  -----------------------  ---------- ---------- ---------
+        /* Overall control/query calls */
+        {"control", "getinfo", &getinfo, true, false, false}, /* uses wallet if enabled */
+        {"control", "help", &help, true, true, false},
+        {"control", "stop", &stop, true, true, false},
+
+        /* P2P networking */
+        {"network", "getnetworkinfo", &getnetworkinfo, true, false, false},
+        {"network", "addnode", &addnode, true, true, false},
+        {"network", "disconnectnode", &disconnectnode, true, true, false},
+        {"network", "getaddednodeinfo", &getaddednodeinfo, true, true, false},
+        {"network", "getconnectioncount", &getconnectioncount, true, false, false},
+        {"network", "getnettotals", &getnettotals, true, true, false},
+        {"network", "getpeerinfo", &getpeerinfo, true, false, false},
+        {"network", "ping", &ping, true, false, false},
+        {"network", "setban", &setban, true, false, false},
+        {"network", "listbanned", &listbanned, true, false, false},
+        {"network", "clearbanned", &clearbanned, true, false, false},
+
+        /* Block chain and UTXO */
+        {"blockchain", "findserial", &findserial, true, false, false},
+        {"blockchain", "getaccumulatorvalues", &getaccumulatorvalues, true, false, false},
+        {"blockchain", "getaccumulatorwitness", &getaccumulatorwitness, true, false, false},
+        {"blockchain", "getmintsinblocks", &getmintsinblocks, true, false, false},
+        {"blockchain", "getserials", &getserials, true, false, false},
+        {"blockchain", "getblockchaininfo", &getblockchaininfo, true, false, false},
+        {"blockchain", "getbestblockhash", &getbestblockhash, true, false, false},
+        {"blockchain", "getblockcount", &getblockcount, true, false, false},
+        {"blockchain", "getblock", &getblock, true, false, false},
+        {"blockchain", "getblockhash", &getblockhash, true, false, false},
+        {"blockchain", "getblockheader", &getblockheader, false, false, false},
+        {"blockchain", "getchaintips", &getchaintips, true, false, false},
+        {"blockchain", "getchecksumblock", &getchecksumblock, false, false, false},
+        {"blockchain", "getdifficulty", &getdifficulty, true, false, false},
+        {"blockchain", "getfeeinfo", &getfeeinfo, true, false, false},
+        {"blockchain", "getmempoolinfo", &getmempoolinfo, true, true, false},
+        {"blockchain", "getrawmempool", &getrawmempool, true, false, false},
+        {"blockchain", "gettxout", &gettxout, true, false, false},
+        {"blockchain", "gettxoutsetinfo", &gettxoutsetinfo, true, false, false},
+        {"blockchain", "invalidateblock", &invalidateblock, true, true, false},
+        {"blockchain", "reconsiderblock", &reconsiderblock, true, true, false},
+        {"blockchain", "verifychain", &verifychain, true, false, false},
+
+        /* Mining */
+        {"mining", "getblocktemplate", &getblocktemplate, true, false, false},
+        {"mining", "getmininginfo", &getmininginfo, true, false, false},
+        {"mining", "getnetworkhashps", &getnetworkhashps, true, false, false},
+        {"mining", "prioritisetransaction", &prioritisetransaction, true, false, false},
+        {"mining", "submitblock", &submitblock, true, true, false},
+        {"mining", "reservebalance", &reservebalance, true, true, false},
+
+#ifdef ENABLE_WALLET
+        /* Coin generation */
+        {"generating", "getgenerate", &getgenerate, true, false, false},
+        {"generating", "gethashespersec", &gethashespersec, true, false, false},
+        {"generating", "setgenerate", &setgenerate, true, true, false},
+        {"generating", "generate", &generate, true, true, false},
+#endif
+
+        /* Raw transactions */
+        {"rawtransactions", "createrawtransaction", &createrawtransaction, true, false, false},
+        {"rawtransactions", "decoderawtransaction", &decoderawtransaction, true, false, false},
+        {"rawtransactions", "decodescript", &decodescript, true, false, false},
+        {"rawtransactions", "getrawtransaction", &getrawtransaction, true, false, false},
+        {"rawtransactions", "sendrawtransaction", &sendrawtransaction, false, false, false},
+        {"rawtransactions", "signrawtransaction", &signrawtransaction, false, false, false}, /* uses wallet if enabled */
+
+        /* Utility functions */
+        {"util", "createmultisig", &createmultisig, true, true, false},
+        {"util", "validateaddress", &validateaddress, true, false, false}, /* uses wallet if enabled */
+        {"util", "verifymessage", &verifymessage, true, false, false},
+        {"util", "estimatefee", &estimatefee, true, true, false},
+        {"util", "estimatepriority", &estimatepriority, true, true, false},
+
+        /* Not shown in help */
+        {"hidden", "invalidateblock", &invalidateblock, true, true, false},
+        {"hidden", "reconsiderblock", &reconsiderblock, true, true, false},
+        {"hidden", "setmocktime", &setmocktime, true, false, false},
+        { "hidden",             "waitfornewblock",        &waitfornewblock,        true,  true,  false  },
+        { "hidden",             "waitforblock",           &waitforblock,           true,  true,  false  },
+        { "hidden",             "waitforblockheight",     &waitforblockheight,     true,  true,  false  },
+
+        /* GIANT features */
+        {"giant", "listmasternodes", &listmasternodes, true, true, false},
+        {"giant", "getmasternodecount", &getmasternodecount, true, true, false},
+        {"giant", "masternodeconnect", &masternodeconnect, true, true, false},
+        {"giant", "createmasternodebroadcast", &createmasternodebroadcast, true, true, false},
+        {"giant", "decodemasternodebroadcast", &decodemasternodebroadcast, true, true, false},
+        {"giant", "relaymasternodebroadcast", &relaymasternodebroadcast, true, true, false},
+        {"giant", "masternodecurrent", &masternodecurrent, true, true, false},
+        {"giant", "masternodedebug", &masternodedebug, tr
