3f40eee2c536ec3bd93518a7df683cffb2eeabf1
3q5y <107141565+3q5y@users.noreply.github.com>
2022-12-20 08:58:20 +0000

    Fixing broken link

--- src/zgic/zerocoin.h
+++ src/zgic/zerocoin.h
@@ -127,4 +127,137 @@ public:
     }
     
     // Copy another CZerocoinMint
-    inline CZerocoinMint& operator=(const 
+    inline CZerocoinMint& operator=(const CZerocoinMint& other) {
+        denomination = other.GetDenomination();
+        nHeight = other.GetHeight();
+        value = other.GetValue();
+        randomness = other.GetRandomness();
+        serialNumber = other.GetSerialNumber();
+        txid = other.GetTxHash();
+        isUsed = other.IsUsed();
+        version = other.GetVersion();
+        privkey = other.GetPrivKey();
+        return *this;
+    }
+    
+    // why 6 below (SPOCK)
+    inline bool checkUnused(int denom, int Height) const {
+        if (IsUsed() == false && GetDenomination() == denomination && GetRandomness() != 0 && GetSerialNumber() != 0 && GetHeight() != -1 && GetHeight() != INT_MAX && GetHeight() >= 1 && (GetHeight() + 6 <= Height)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(isUsed);
+        READWRITE(randomness);
+        READWRITE(serialNumber);
+        READWRITE(value);
+        READWRITE(denomination);
+        READWRITE(nHeight);
+        READWRITE(txid);
+
+        bool fVersionedMint = true;
+        try {
+            READWRITE(version);
+        } catch (...) {
+            fVersionedMint = false;
+        }
+
+        if (version > CURRENT_VERSION) {
+            version = 1;
+            fVersionedMint = false;
+        }
+
+        if (fVersionedMint)
+            READWRITE(privkey);
+    };
+};
+
+class CZerocoinSpend
+{
+private:
+    CBigNum coinSerial;
+    uint256 hashTx;
+    CBigNum pubCoin;
+    libzerocoin::CoinDenomination denomination;
+    unsigned int nAccumulatorChecksum;
+    int nMintCount; //memory only - the amount of mints that belong to the accumulator this is spent from
+
+public:
+    CZerocoinSpend()
+    {
+        SetNull();
+    }
+
+    CZerocoinSpend(CBigNum coinSerial, uint256 hashTx, CBigNum pubCoin, libzerocoin::CoinDenomination denomination, unsigned int nAccumulatorChecksum)
+    {
+        this->coinSerial = coinSerial;
+        this->hashTx = hashTx;
+        this->pubCoin = pubCoin;
+        this->denomination = denomination;
+        this->nAccumulatorChecksum = nAccumulatorChecksum;
+    }
+
+    void SetNull()
+    {
+        coinSerial = 0;
+        hashTx = 0;
+        pubCoin = 0;
+        denomination = libzerocoin::ZQ_ERROR;
+    }
+
+    CBigNum GetSerial() const { return coinSerial; }
+    uint256 GetTxHash() const { return hashTx; }
+    void SetTxHash(uint256 hash) { this->hashTx = hash; }
+    CBigNum GetPubCoin() const { return pubCoin; }
+    libzerocoin::CoinDenomination GetDenomination() const { return denomination; }
+    unsigned int GetAccumulatorChecksum() const { return this->nAccumulatorChecksum; }
+    uint256 GetHash() const;
+    void SetMintCount(int nMintsAdded) { this->nMintCount = nMintsAdded; }
+    int GetMintCount() const { return nMintCount; }
+ 
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
+        READWRITE(coinSerial);
+        READWRITE(hashTx);
+        READWRITE(pubCoin);
+        READWRITE(denomination);
+        READWRITE(nAccumulatorChecksum);
+    };
+};
+
+class CZerocoinSpendReceipt
+{
+private:
+    std::string strStatusMessage;
+    int nStatus;
+    int nNeededSpends;
+    std::vector<CZerocoinSpend> vSpends;
+
+public:
+    void AddSpend(const CZerocoinSpend& spend);
+    std::vector<CZerocoinSpend> GetSpends();
+    void SetStatus(std::string strStatus, int nStatus, int nNeededSpends = 0);
+    std::string GetStatusMessage();
+    int GetStatus();
+    int GetNeededSpends();
+};
+
+/**
+ * Wrapped serials attack inflation, only for mainnet.
+ * FUTURE: Move this to another file..
+ * @param denom
+ * @return
+ */
+int GetWrapppedSerialInflation(libzerocoin::CoinDenomination denom);
+
+int64_t GetWrapppedSerialInflationAmount();
+
+#endif //GIANT_ZEROCOIN_H
---
+++ src/zmq/zmqnotificationinterface.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2015 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "zmqnotificationinterface.h"
+#include "zmqpublishnotifier.h"
+
+#include "version.h"
+#include "main.h"
+#include "streams.h"
+#include "util.h"
+
+void zmqError(const char *str)
+{
+    LogPrint("zmq", "zmq: Error: %s, errno=%s\n", str, zmq_strerror(errno));
+}
+
+CZMQNotificationInterface::CZMQNotificationInterface() : pcontext(NULL)
+{
+}
+
+CZMQNotificationInterface::~CZMQNotificationInterface()
+{
+    Shutdown();
+
+    for (std::list<CZMQAbstractNotifier*>::iterator i=notifiers.begin(); i!=notifiers.end(); ++i)
+    {
+        delete *i;
+    }
+}
+
+CZMQNotificationInterface* CZMQNotificationInterface::CreateWithArguments(const std::map<std::string, std::string> &args)
+{
+    CZMQNotificationInterface* notificationInterface = NULL;
+    std::map<std::string, CZMQNotifierFactory> factories;
+    std::list<CZMQAbstractNotifier*> notifiers;
+
+    factories["pubhashblock"] = CZMQAbstractNotifier::Create<CZMQPublishHashBlockNotifier>;
+    factories["pubhashtx"] = CZMQAbstractNotifier::Create<CZMQPublishHashTransactionNotifier>;
+    factories["pubhashtxlock"] = CZMQAbstractNotifier::Create<CZMQPublishHashTransactionLockNotifier>;
+    factories["pubrawblock"] = CZMQAbstractNotifier::Create<CZMQPublishRawBlockNotifier>;
+    factories["pubrawtx"] = CZMQAbstractNotifier::Create<CZMQPublishRawTransactionNotifier>;
+    factories["pubrawtxlock"] = CZMQ
